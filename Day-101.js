384c84156d82cf7540eafef1f7622eeec1e46546
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-01-08 11:34:03 +0500

    File no 1 1️⃣

---
+++ Day-101.js
@@ -0,0 +1,44 @@
+// Let's revise all the basics of javascript.
+/*
+
+1. Functions
+2. Objects
+3. Arrays 2d and statis
+4. Loops
+5. Array Methods
+6. String Methods
+7. Date & Math Method
+*/
+
+// Topic #1 -- Keyword vs Word
+/*
+
+    Any thing that has a particular meaning in JS language called Keyword i.e (if, else, else if, case, switch, break, etc).
+    Any thing that not has a particular meaning in js language called Word i.e (my_Name, my_Age, a, b, c etc).
+*/
+
+// Topic #2 -- Variable & Constants
+/*
+
+    Variales are used to store data in your code and you use it for performing operations, You can change variable values and reintiallize 
+    them as per requiremnts. There are two ways by which you can create variable in JS i.e (let, var)
+
+    Constants are also used to store data in your code but you never change or reinitalize their values. You can create constant which the help
+    of const keyword in JavaScript Language.
+
+*/
+
+// Topic #3 -- Undefined & Not defined.
+
+
+    //Lets understand this the help of an example.
+
+    console.log(a);
+    var a =  "Muhammad Shakir";
+
+    //output is -- undefined 
+
+    console.log(b);
+    
+    //output is -- b is not defined 
+
