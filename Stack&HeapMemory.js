688a4d3762fd000a410e6530894c33f781ba3d64
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-09 13:12:33 +0500

    File no 7 Stack & Heap memory

---
+++ Stack&HeapMemory.js
@@ -0,0 +1,26 @@
+// Stack & Heap memory
+/*
+    Stack => use with (Primitive Datatype)
+    Heap memory => use with (Non-Primitive Datatype)
+*/
+
+// Stack memory example.
+let myMail = "chaudhryshakir@google.com";
+let mainMail = myMail;
+mainMail = "MuhammadShakirdev@google.com";
+
+console.log(myMail);
+console.log(mainMail);
+
+// Heap memory example.
+let userOne = {
+    mail: "xyz@google.com",
+    city: "karachi",
+    gender: "male"
+};
+
+// Stack memory example.
+let userTwo = userOne;
+userTwo.mail = "Shakir.dev@google.com";
+console.log(userOne);
+console.log(userTwo);
