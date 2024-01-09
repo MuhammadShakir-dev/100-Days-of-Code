a2b5d9b8bc6a1b5c2dfe6ef936ee9802a3aac609
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-09 11:54:48 +0500

    File no 5 Datatypes with notes

---
+++ Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as a newer version through JS engine.
+
+// alert(3+3) // we are using node.js not the browser
+
+
+
+// Premitive Datatypes in JS
+/*
+    numbers => rang is 0 - power of 53
+    bigint => higher values then power of 53
+    string => use ""
+    boolean => true / false
+    null => standalone value but not undefined 
+    undefined => not assigned value
+    symbol =>  use for unique representation
+*/
+
+// Reference Data types
+
+/*
+    objects 
+    funcitons
+    arrays
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
