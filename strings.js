cdbe9287dc40a1ae02a40835d77522debfb04263
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-10 13:47:32 +0500

    File no 8 strings in JavaScript

--- strings.js
+++ strings.js
@@ -1,1 +1,15 @@
 // Strings in JavaScript
+
+// old way to wirte strings in JS.
+
+let myName = "Myself Muhammad Shakir";
+let myProfession = "I'm a Software Eng";
+console.log(myName + " " + myProfession);
+
+// New way to wirte strings in JS.
+console.log(`${myName} & ${myProfession}`);
+
+// There is another way by which you can write strings.
+let myEmail = new String('MuhammadShakirdev@gmail.com');
+console.log(myEmail); 
+
