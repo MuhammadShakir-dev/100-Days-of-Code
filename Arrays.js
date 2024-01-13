041a038c9546cd95731799bf4d1b234ee7e5fcb4
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-13 12:17:13 +0500

    File no 10 Arrays in JavaScript

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+// Let's learn about arrays in JS
+
+let ary1 = [0, 1, 2, 3, 4, 5];
+let ary2 = ["Muhammad Shakir", 23, "Male"];
+let ary3 = new Array(1,2,3,4,5)
+console.log(ary1);
+console.log(ary2);
+console.log(ary3);
