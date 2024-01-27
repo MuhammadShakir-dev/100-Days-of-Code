1322f374145548b5c88af5cc5da6f3d113b1793d
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-27 11:41:06 +0500

    IFEE in JavaScript

---
+++ IFFE.js
@@ -0,0 +1,19 @@
+// Imediatly Invoked Function Expressions IFFE.
+
+// What is IFEE?
+// IFEE is Imddiatly invoked/execute itself after the function defintion.
+
+// Why we use IFEE?
+// Some time we want to get our function be poluted by some variables in Globel scope so to get rid from this
+// we use IFEE.
+
+(function add(num1, num2) {
+    // this is named IFEE because it has a name : add
+    console.log(num1 + num2);
+})(3, 4);
+
+(() => {
+    // this is simple IFEE
+    console.log("Database Connected successfully");
+})();
+
