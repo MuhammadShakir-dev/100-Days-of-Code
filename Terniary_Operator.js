c6ba738672cf064d41df30dfe15196e5a506235b
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-30 15:38:36 +0500

    Terniary Operator in JS

---
+++ Terniary_Operator.js
@@ -0,0 +1,10 @@
+/*
+Note down one thing that There is a difference between Null Coaleascing and Terniary Operator.
+
+syntax : condition ? true : false
+
+Now terniary operator has different work, goal and syntax as compaire to Null coasleing operator
+*/
+
+let bitconPrice = 43408;
+bitconPrice <= 42000 ? console.log("Price is Decrease") : console.log("Price is Increase");
