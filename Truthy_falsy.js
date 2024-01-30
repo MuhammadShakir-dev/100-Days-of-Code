4749f302a618e4d93d5d2a391ded70168da5c9ed
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-30 14:47:23 +0500

    Truthy & Falsy values in JS

---
+++ Truthy_falsy.js
@@ -0,0 +1,13 @@
+const user_Email = "Shakir@google.com";
+const user_gender = "";
+
+if (user_gender) {
+    console.log("Got your email");
+} else { 
+    console.error("Opps! Something went worng");
+}
+
+// List of faly values in JS
+// false, 0, -0, BigInt 0n, Null, undefined, Nan, "" 
+
+// the above all are falsy values other then that all are truthy values
