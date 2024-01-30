ffbb5d25414aede7db113f59b850a037a3a49f27
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-30 19:59:26 +0500

    For loop in JS

--- Loops in JavaScript/For_loop.js
+++ Loops in JavaScript/For_loop.js
@@ -27,8 +27,33 @@ for (let i = 1; i <= 10; i++) {
 // now lets print 1 to 10 tables
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`Table of ${i}`)
+    // console.log(`Table of ${i}`)
     for (let j = 1; j <= 10; j++) { 
-        console.log(i + "*" + j + "=" + i*j);
+        // console.log(i + "*" + j + "=" + i*j);
     }
+}
+
+// now lets print an array elements with for loop
+const my_Array = ["Flash", "Batman", "Superman", "Spiderman"];
+for (let i = 0; i < my_Array.length; i++) { 
+    console.log(my_Array[i]);
+}
+
+// Now lets see the use of break keyword in loop
+for (let i = 1; i <= 10; i++) { 
+    if (i == 5) { 
+        // console.log(`${i} is detected`);
+        break
+    }
+    // console.log(i);
+}
+
+
+// Now lets see continue keyword working
+for (let i = 1; i <= 10; i++) { 
+    if (i == 5) { 
+        console.log(`${i} is detected`);
+        continue
+    }
+    console.log(i);
 }
