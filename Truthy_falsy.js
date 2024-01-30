a984b86d97dd45b75d266875825a77e11c0cf4b3
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-01-30 15:15:15 +0500

    Truthy and Falsy in JS

--- Truthy_falsy.js
+++ Truthy_falsy.js
@@ -10,4 +10,17 @@ if (user_gender) {
 // List of faly values in JS
 // false, 0, -0, BigInt 0n, Null, undefined, Nan, "" 
 
-// the above all are falsy values other then that all are truthy values
+// the above all are falsy values other then that all are truthy values.
+
+
+// Lets see how we check that our array or object is empty
+
+const myArray = [];
+if (myArray.length == 0) { 
+    console.log("Array is empty");
+}
+
+const myObj = {};
+if (Object.keys(myObj).length == 0) { 
+    console.log("Object is empty")
+}
