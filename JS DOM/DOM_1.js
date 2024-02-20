12aa6aff2fadd63b4a88ea513b938eb7f2389c0d
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-02-20 21:39:25 +0500

    DOM Selectores , nodelists and HTMLCollection

---
+++ JS DOM/DOM_1.js
@@ -0,0 +1,30 @@
+// Lets build our understanding with JS DOM
+
+// This will give the exact id name of the id you're looking for
+document.getElementById("title").id
+
+
+// This will give you the classname of the class you're looking for
+document.getElementById("title").className
+
+// You can also find the attributes inside of an html tag 
+document.getElementById("title").getAttribute('class')
+
+// You cans also set attribute inside of an HTML tag but notedown onething that this will overwrite your class name
+// you can avoid this by giving them an extra name
+
+document.getElementById("title").setAttribute('class', 'test'); // this will overwrite heading => test
+document.getElementById('title').setAttribute('class', 'test heading') // this is the correct way of doing this
+
+
+// lets store the getElementById in a variable
+let title = document.getElementById("title");
+
+// You can also set styles in this using the above variable
+title.style.backgroundColor = 'lightGreen'
+title.style.color = 'black'
+title.style.textAlign = 'center'
+
+
+
+
