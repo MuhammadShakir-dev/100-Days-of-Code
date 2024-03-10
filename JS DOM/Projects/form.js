bdc2c29fbbe8854a0d6292b455900555641338ea
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-03-10 13:08:58 +0500

    form JS code

---
+++ JS DOM/Projects/form.js
@@ -0,0 +1,10 @@
+const req = document.querySelector('form');
+const signup = document.querySelector('a');
+const btn = document.querySelector('button');
+
+req.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+
+
