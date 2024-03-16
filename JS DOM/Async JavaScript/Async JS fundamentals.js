960c4ad674a47aab54f3f7828919f6f1a3144fa5
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-03-16 15:42:44 +0500

    Async in JavaScript (Theory)

---
+++ JS DOM/Async JavaScript/Async JS fundamentals.js
@@ -0,0 +1,4 @@
+// you can access the diagram by just clicking on the below link
+
+const FigmaFileLink = `https://www.figma.com/file/rS9u77fXFoqR1WLSBBeW2x/JavaScript-Async-Code?type=whiteboard&node-id=0%3A1&t=Zqn4jpzsATqTBf8A-1`;
+
