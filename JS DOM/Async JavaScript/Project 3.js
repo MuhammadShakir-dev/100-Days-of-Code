a519ffe5d76eff2931bbf4c23dbdb157aed1de63
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-03-17 12:53:42 +0500

    JavaScript source code of Project 3

--- JS DOM/Async JavaScript/Project 3.js
+++ JS DOM/Async JavaScript/Project 3.js
@@ -1,1 +1,23 @@
+const container = document.querySelector(".container");
 
+window.addEventListener("keydown", (e) => { 
+    container.innerHTML =
+    
+    `
+    <div class = color>
+        <table border = 1dss>
+        <tr>
+          <th>Key</th>
+          <th>Keycode</th>
+          <th>Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === " " ? "Space" : e.key}</td>
+          <td>${e.keyCode}</td>
+          <td>${e.code}</td>
+        </tr>
+      </table>
+    </div>
+    
+    `
+} )
