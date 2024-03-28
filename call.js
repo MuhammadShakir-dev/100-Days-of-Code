7535eca63fe28926bded1e9d47c3bd0d56244b5b
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-03-28 14:26:39 +0500

    Update call.js

--- call.js
+++ call.js
@@ -1,1 +1,14 @@
+// lets understand call and this in JavaScript  
 
+function setUserName(name) { 
+    this.name = name;
+}
+
+function createUser(name, email, password) { 
+    setUserName.call(this, name);
+    this.email = email;
+    this.password = password;
+}
+
+const userOne = new createUser("Muhammad Shakir", "shakirxyz.com", "123124");
+console.log(userOne); 
