3c61a8894d994ff099ade71a6432f39dfed63940
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-03-30 17:02:18 +0500

    Update object_based_get_set.js

--- object_based_get_set.js
+++ object_based_get_set.js
@@ -1,1 +1,16 @@
 
+const anotherUser = {
+    _username: "shakir",
+    _email: "shakir@gmail.com",
+
+    get username() { 
+        return this._username.toUpperCase()
+    },
+
+    set username(value) {
+        this._username = value;
+    }
+} 
+
+let userTwo = Object.create(anotherUser);
+console.log(userTwo.username);
