eaf92ca4bccd7e4c7e13b6625764103059c4145e
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-03-30 16:59:51 +0500

    Update function_based_get_set.js

--- function_based_get_set.js
+++ function_based_get_set.js
@@ -1,1 +1,25 @@
+function createUser(name, email) { 
+    this.name = name;
+    this.email = email;
 
+    Object.defineProperty(this, "name",{
+        get: function () {
+            return this._name.toUpperCase()
+        },
+        set: function (value) { 
+            this._name = value;
+        }
+    });
+
+    Object.defineProperty(this, "email", {
+        get: function () { 
+            return `${this._email.toUpperCase()}.JS` 
+        },
+
+        set: function (value) { 
+            return this._email = value
+        }
+    });
+}
+
+const newUser = new createUser("shakir", "shakir@dev.com");
